447a7be17adaed688f97769bd5b75d4a86ac3433
zlin <1562627895@qq.com>
2021-11-24 22:16:25 +0800

    config server, config client

---
+++ config/config-client/src/main/java/com/zlin/springcloud/ConfigClientApp.java
@@ -0,0 +1,16 @@
+package com.zlin.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zlin
+ * @date 20211124
+ */
+@SpringBootApplication
+public class ConfigClientApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApp.class, args);
+    }
+}
---
+++ config/config-server/src/main/java/com/zlin/springcloud/ConfigServerApp.java
@@ -0,0 +1,25 @@
+package com.zlin.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author zlin
+ * @date 20211124
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApp {
+
+//    获取配置文件的不同URL姿势，都是GET请求
+//    http://localhost:60000/{label}/{application}-{profile}.json
+//    以上后缀可以换成.yml, .properties，如果不指定{label}的话默认用master
+//
+//    http://localhost:60000/{application}/{profile}/{label}
+//    如果不指定{label}的话默认用master
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApp.class, args);
+    }
+}
